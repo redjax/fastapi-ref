2405f4b8410cc591df9eaeb8ff4e3ed138553d83
redjax <none@none.com>
2023-07-29 02:35:53 -0400

    feat(alembic): Add alembic migrations
    Add & configure Alembic.
    
    Write notes in README

--- example_apps/app_3/app/domain/item/schemas.py
+++ example_apps/app_3/app/domain/item/schemas.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 class ItemBase(BaseModel):
     name: str = Field(default=None, description="The name of the Item")
     description: str = Field(default=None, description="The description of the Item")
+    quantity: int = Field(default=0, description="Number of Items in inventory")
 
 
 class ItemCreate(ItemBase):
